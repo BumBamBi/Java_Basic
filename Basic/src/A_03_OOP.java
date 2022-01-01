038c4556b122dd6347a6ca73ff1da5c4e49c1699
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-01 19:16:37 +0900

    상속

--- Basic/src/A_03_OOP.java
+++ Basic/src/A_03_OOP.java
@@ -1,8 +1,26 @@
 public class A_03_OOP {
     public static void main(String[] args) throws Exception {
+        // 객체 생성
         A_03_OOP_car mycar = new A_03_OOP_car();
+        
+        // set
         mycar.setCarName("LEE");
         String tempName = mycar.getCarName();
+        
+        // 출력
         System.out.println(tempName);
+
+        /* ------------------------------------------------- */
+        // 상속
+        A_03_OOP_car_taxi mytaxi = new A_03_OOP_car_taxi();
+        
+        // set
+        mytaxi.setFare(1000);
+        int tempFare = mytaxi.getFare();
+
+        // 출력
+        System.out.println(tempFare);
+
+
     }
 }
---
+++ Basic/src/A_03_OOP_car_taxi.java
@@ -0,0 +1,11 @@
+public class A_03_OOP_car_taxi extends A_03_OOP_car {
+    int fare;
+
+    public int getFare() {
+        return fare;
+    }
+
+    public void setFare(int fare) {
+        this.fare = fare;
+    }
+}
