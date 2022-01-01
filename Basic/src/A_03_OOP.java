27c6227995c1e3a663f407ed63adaee4c38ca0f1
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-01 19:08:46 +0900

    객체지향프로그래밍 기초

---
+++ Basic/src/A_03_OOP.java
@@ -0,0 +1,8 @@
+public class A_03_OOP {
+    public static void main(String[] args) throws Exception {
+        A_03_OOP_car mycar = new A_03_OOP_car();
+        mycar.setCarName("LEE");
+        String tempName = mycar.getCarName();
+        System.out.println(tempName);
+    }
+}
---
+++ Basic/src/A_03_OOP_car.java
@@ -0,0 +1,17 @@
+public class A_03_OOP_car {
+    String carName;
+    int curSpeed;
+
+    public String getCarName() {
+        return carName;
+    }
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+    public int getCurSpeed() {
+        return curSpeed;
+    }
+    public void setCurSpeed(int curSpeed) {
+        this.curSpeed = curSpeed;
+    }
+}
