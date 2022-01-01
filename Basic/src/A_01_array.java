1f945091000959bffb2e866f10d9cad61e890829
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-01 18:48:31 +0900

    배열

---
+++ Basic/src/A_01_array.java
@@ -0,0 +1,18 @@
+public class A_01_array {
+    public static void main(String[] args) throws Exception {
+        int[] arr1;
+        arr1 = new int[5];
+        System.out.println(arr1[0]);
+
+        int[] arr2 = {1, 2, 3,4, 5};
+        System.out.println(arr2[0]);
+
+
+        // 2차원 배열
+        int[][] arr3 = new int[3][];
+        // 각각 다른 길이를 가지도록 할 수 있음.
+        arr3[0] = new int[3];
+        arr3[1] = new int[6];
+        arr3[2] = new int[9];
+    }
+}
