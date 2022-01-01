36fd4e80c24e904ff848727386488d87414037b0
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-01 19:29:20 +0900

    접근자 public, private

---
+++ Basic/src/A_04_public_private.java
@@ -0,0 +1,18 @@
+public class A_04_public_private {
+    public int name;
+    private int SSN;
+
+    
+    public int getName() {
+        return name;
+    }
+    public void setName(int name) {
+        this.name = name;
+    }
+    public int getSSN() {
+        return SSN;
+    }
+    public void setSSN(int sSN) {
+        SSN = sSN;
+    }
+}
