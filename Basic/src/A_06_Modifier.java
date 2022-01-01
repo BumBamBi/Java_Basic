9dbd45d97601e66a60404a77393f5bf073c472cc
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-01 19:46:39 +0900

    생성자

---
+++ Basic/src/A_06_Modifier.java
@@ -0,0 +1,14 @@
+public class A_06_Modifier {
+    
+    // 클래스에 사용되는 접근자
+    // public   - 모두 접근 가능
+    // final    - 자식 객체 생성(상속) 불가
+    // abstract - 객체 생성 불가
+
+
+    // 멤버변수에 사용하는 접근자
+    // private      - 해당 클래스
+    // (default)    - +동일 패키지
+    // protected    - +하위 클래스
+    // public       - 모두 접근 가능
+}
