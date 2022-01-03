e174166532dbb681b4e58c637eae22767d5c9822
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-03 20:31:34 +0900

    생성자(Constructor) 설명 코드 추가

---
+++ src/A_05_Constructor/Employee.java
@@ -0,0 +1,11 @@
+package A_05_Constructor;
+
+public class Employee {
+    public String name;
+    private int SSN;
+
+    public Employee(String name, int SSN){
+        this.name = name;
+        this.SSN = 1234;
+    }
+}
--- src/A_05_Constructor/mainClass.java
+++ src/A_05_Constructor/mainClass.java
@@ -1,13 +1,13 @@
 package A_05_Constructor;
 
 public class mainClass {
-    public String name;
-    private int SSN;
-
-    public mainClass(String name, int SSN){
-        this.name = "init name";
-        this.SSN = 1234;
+    // 반환형이 없다
+    // 생성자는 클래스와 같은 이름으로 선언됨
+    // 이름은 같게, 매개변수를 다르게 하여 중복정의(Overloading)가능
+    // 접근 권한자로 (public, protected, private)이 있다.
+    
+    public static void main(String args[]){
+        Employee E1 = new Employee("이름", 123);
+        // 생성자의 매개변수 만큼 항상 값을 넣어서 초기화 해야함.
     }
-
-    //setter getter
 }
